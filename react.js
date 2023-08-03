b4d1e99e8c46a41dec5e6ce18dd149b7ec580571
Guilherme <guilherme@alume.com>
2023-08-03 10:39:56 -0300

    format react config

--- react.js
+++ react.js
@@ -4,51 +4,50 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'standard',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "standard",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    "react",
-    "@typescript-eslint",
-    "prettier"
-  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': ["error", {
-      'printWidth': 100,
-      'tabWidth': 2,
-      'singleQuote': true,
-      'trailingComma': 'all',
-      'arrowParens': 'always',
-      'semi': true,
-      'endOfLine': 'auto',
-      'no-var': 'error',
-      'indent': 'error',
-      'no-multi-spaces': 'error',
-      'space-in-parens': 'error',
-      'no-multiple-empty-lines': 'error',
-      'prefer-const': 'error',
-      'no-use-before-define': 'error',
-    }],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: "all",
+        arrowParens: "always",
+        semi: true,
+        endOfLine: "auto",
+        "no-var": "error",
+        indent: "error",
+        "no-multi-spaces": "error",
+        "space-in-parens": "error",
+        "no-multiple-empty-lines": "error",
+        "prefer-const": "error",
+        "no-use-before-define": "error",
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
-    'import/parsers': {
-      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
-  }
-}
+  },
+};
